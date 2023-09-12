6cd63c5831bdb74d3b17f728e579079c58fb76b6
atul <mauryaatul328@gmail.com>
2023-09-12 10:19:50 +0530

    first commit

--- src/Components/payment.js
+++ src/Components/payment.js
@@ -28,59 +28,66 @@ class Payment extends Component {
     return (
       <div className="App">
         <div className="payment-card w-100">
-          <div className="card-details">
-            <h2>Complete Payment</h2>
-            <form className="payment-form">
-              <input
-                type="text"
-                className="payment-input"
-                id="card-number"
-                name="cardNumber"
-                value={this.state.cardNumber}
-                placeholder="Card Number"
-                onChange={this.handleInputChange}
-                required
-              />
-              <br />
-              <br />
-              <input
-                type="text"
-                className="payment-input"
-                id="expiry"
-                name="expiry"
-                value={this.state.expiry}
-                onChange={this.handleInputChange}
-                placeholder="MM/YY"
-                required
-              />
-              <br />
-              <br />
-              <input
-                type="text"
-                className="payment-input"
-                id="cvv"
-                name="cvv"
-                placeholder="CVV"
-                value={this.state.cvv}
-                onChange={this.handleInputChange}
-                required
-              />
-              <br />
-              <br />
-            </form>
-            <button
-              type="button"
-              className="payment-confirm"
-              onClick={this.handleConfirmPayment}
-            >
-              Confirm Payment
-            </button>
-          </div>
-          <div className="order-summary mt-2 py-5">
-            <h2>Order Summary</h2>
-            <p>Plan Name: {selectedPlan}</p>
-            <p>Billing Cycle: {plan}</p>
-            <p>Plan Price: {planPrice}</p>
+          <div className="row">
+            <div className="col-md-6 pb-3">
+              <div className="card-details">
+                <h2>Complete Payment</h2>
+                <form className="payment-form row">
+                  <input
+                    type="text"
+                    className="payment-input"
+                    id="card-number"
+                    name="cardNumber"
+                    value={this.state.cardNumber}
+                    placeholder="Card Number"
+                    onChange={this.handleInputChange}
+                    required
+                  />
+                  <br />
+                  <br />
+                  <input
+                    type="text"
+                    className="payment-input"
+                    id="expiry"
+                    name="expiry"
+                    value={this.state.expiry}
+                    onChange={this.handleInputChange}
+                    placeholder="MM/YY"
+                    required
+                  />
+                  <br />
+                  <br />
+                  <input
+                    type="text"
+                    className="payment-input"
+                    id="cvv"
+                    name="cvv"
+                    placeholder="CVV"
+                    value={this.state.cvv}
+                    onChange={this.handleInputChange}
+                    required
+                  />
+                  <br />
+                  <br />
+                </form>
+                <button
+                  type="button"
+                  className="payment-confirm"
+                  onClick={this.handleConfirmPayment}
+                >
+                  Confirm Payment
+                </button>
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="order-summary mt-2 py-5">
+                <h2>Order Summary</h2>
+                <p>Plan Name: {selectedPlan}</p>
+                <p>Billing Cycle: {plan}</p>
+                <p>Plan Price: {planPrice}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
